931fb6051b3bb0ca6ace49d913aade5f1fb3f494
Jonathan Xue <jonathanxue50@gmail.com>
2021-02-11 15:48:48 -0600

    Typewriter

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
-var video = document.querySelector("#webcam #video");
+// Typewriter
+const text = "It'd be a shame for a girl like you to spend it alone. :("
+const typingDelay = 75;
+
+let textSpan = document.querySelector(".typewriter .typewriter-text");
+let charIndex = 0;
+function type() {
+  if (charIndex < text.length) {
+    textSpan.textContent += text.charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  }
+}
+type();
+
+// Webcam
+let video = document.querySelector("#webcam #video");
 if (navigator.mediaDevices.getUserMedia) {
   navigator.mediaDevices.getUserMedia({ video: true })
     .then(function (stream) {
