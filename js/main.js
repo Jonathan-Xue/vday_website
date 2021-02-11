966c9f5649322be6d6fb0a1b4f9ab31fd0097b7d
Jonathan Xue <jonathanxue50@gmail.com>
2021-02-11 14:10:07 -0600

    Website

--- js/main.js
+++ js/main.js
@@ -1,47 +1,15 @@
-(function($) {
+var video = document.querySelector("#webcam #video");
+if (navigator.mediaDevices.getUserMedia) {
+  navigator.mediaDevices.getUserMedia({ video: true })
+    .then(function (stream) {
+      video.srcObject = stream;
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+}
 
-	"use strict";
-
-	$(function() {
-
-		var $window = $(window),
-			$body = $('body'),
-			$header = $('#header'),
-			$banner = $('#banner');
-
-		// Disable animations/transitions until the page has loaded.
-		$body.addClass('preload');
-		$window.on('load', function() {
-			$body.removeClass('preload');
-		});
-
-		// Smooth Scrolling
-		$('a[href*="#"]')
-			.not('[href="#"]')
-			.not('[href="#0"]')
-			.click(function(event) {
-				if (
-					location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
-					location.hostname == this.hostname
-				) {
-					var target = $(this.hash);
-					target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-					if (target.length) {
-						event.preventDefault();
-						$('html, body').animate({
-							scrollTop: target.offset().top
-						}, 1000, function() {
-							var $target = $(target);
-							$target.focus();
-							/*if ($target.is(":focus")) {
-								return false;
-							} else {
-								$target.attr('tabindex', '-1');
-								$target.focus();
-							};*/
-						});
-					}
-				}
-			});
-	});
-})(jQuery);
+document.querySelector("#webcam #overlay").addEventListener("click", () => {
+  document.querySelector("#webcam #overlay").style.visibility = "hidden";
+  document.querySelector("#webcam-caption").style.visibility = "visible";
+});
